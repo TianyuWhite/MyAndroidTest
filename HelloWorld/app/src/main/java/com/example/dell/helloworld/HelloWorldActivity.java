4ece508394d5c6ad403d05cc144e289842857507
TianyuWhite <245874567@qq.com>
2018-02-02 22:45:11 +0800

    HelloWorld
    HelloWorld

--- HelloWorld/app/src/main/java/com/example/dell/helloworld/HelloWorldActivity.java
+++ HelloWorld/app/src/main/java/com/example/dell/helloworld/HelloWorldActivity.java
@@ -2,6 +2,7 @@ package com.example.dell.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class HelloWorldActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class HelloWorldActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hello_world_layout);
+        Log.d("HelloWorldActivity", "onCreate execute");
+        Log.d("data","onCreate execute");
     }
 }
