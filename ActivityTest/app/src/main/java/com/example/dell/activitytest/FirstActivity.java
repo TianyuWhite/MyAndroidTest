94140e11427f3ca3cf00801932b59aa50699d6ee
TianyuWhite <245874567@qq.com>
2018-02-04 23:51:55 +0800

    ActivityTest

--- ActivityTest/app/src/main/java/com/example/dell/activitytest/FirstActivity.java
+++ ActivityTest/app/src/main/java/com/example/dell/activitytest/FirstActivity.java
@@ -8,5 +8,6 @@ public class FirstActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
     }
 }
