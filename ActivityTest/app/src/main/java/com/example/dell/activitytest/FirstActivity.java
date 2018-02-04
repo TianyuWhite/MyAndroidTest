fac6ae5ee03135c7100ccde0152b3f9236211ccd
TianyuWhite <245874567@qq.com>
2018-02-04 23:38:55 +0800

    ActivityTest
    第二章第一个程序

---
+++ ActivityTest/app/src/main/java/com/example/dell/activitytest/FirstActivity.java
@@ -0,0 +1,12 @@
+package com.example.dell.activitytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
