5beffabe5d04eab0b7c3e818787d6e35206e9ea1
TianyuWhite <245874567@qq.com>
2018-02-19 23:54:30 +0800

    UIWidgetTest
    UIWidgetTest

--- UIWidgetTest/app/src/main/java/com/example/dell/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/example/dell/uiwidgettest/MainActivity.java
@@ -4,8 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //通过点击按钮来获取EditText中输入内容
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });*/
 
+        //通过点击按钮来获取EditText中输入内容,findViewById()得到EditText的实例
+        editText = (EditText) findViewById(R.id.edit_text);
         //使用实现接口的方式来注册
         button.setOnClickListener(this);
     }
@@ -29,7 +36,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch(view.getId()){
             case R.id.button:
-                //...
+                //通过点击按钮来获取EditText中输入内容
+                String inputText = editText.getText().toString(); //getText()获取输入的内容，toString()转换成字符串，Toast显示作用
+                Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_SHORT).show();
                 break;
             default:
                 break;
