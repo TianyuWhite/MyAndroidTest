58a21d2c76a226179ae1ad5bfbf95fb200770ff3
TianyuWhite <245874567@qq.com>
2018-02-27 22:35:55 +0800

    ListViewTest
    ListViewTest

--- ListViewTest/app/src/main/java/com/example/dell/listviewtest/FruitAdapter.java
+++ ListViewTest/app/src/main/java/com/example/dell/listviewtest/FruitAdapter.java
@@ -25,13 +25,40 @@ public class FruitAdapter extends ArrayAdapter <Fruit> {
     }
 
     public View getView(int position, View converView, ViewGroup parent){
+        //converView用于将之前加载好的布局进行缓存
         Fruit fruit = getItem(position);        //获取当前项的Fruit实例
-        View view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+        //View view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
         //LayoutInflater为子项加载传入的布局
-        ImageView fruitImage = (ImageView)view.findViewById(R.id.fruit_image);
+
+        View view;
+        ViewHolder viewHolder;      //ViewHolder为一个内部类,对控件实例进行缓存
+        //现在在getView方法中进行判断， 若convertView为null， 则加载布局，不为null则重用
+        if(converView == null){
+            //view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+            view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+            viewHolder = new ViewHolder();  //为null时创建实例，实例都存放在ViewHolder里
+            viewHolder.fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+            viewHolder.fruitName = (TextView) view.findViewById(R.id.fruit_name);
+            view.setTag(viewHolder);        //setTag()将ViewHolder存在View中
+        }
+        else{
+            view = converView;
+            viewHolder = (ViewHolder) view.getTag();    //getTag()重新获取ViewHolder
+        }
+
+        /*ImageView fruitImage = (ImageView)view.findViewById(R.id.fruit_image);
         TextView fruitName = (TextView) view.findViewById(R.id.fruit_name);
         fruitImage.setImageResource(fruit.getImageId());
-        fruitName.setText(fruit.getName());
+        fruitName.setText(fruit.getName());*/
+
+        //这样所有实例都缓存在ViewHolder里了，无需每次都通过findViewById()来获取控件实例
+        viewHolder.fruitImage.setImageResource(fruit.getImageId());
+        viewHolder.fruitName.setText(fruit.getName());
         return view;
+    }
+
+    private class ViewHolder {
+        ImageView fruitImage;
+        TextView fruitName;
     }
 }
--- ListViewTest/app/src/main/java/com/example/dell/listviewtest/MainActivity.java
+++ ListViewTest/app/src/main/java/com/example/dell/listviewtest/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.dell.listviewtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +31,14 @@ public class MainActivity extends AppCompatActivity {
      //   ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, data);
         ListView listView = (ListView) findViewById(R.id.list_view);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Fruit fruit = fruitList.get(position);
+                Toast.makeText(MainActivity.this, fruit.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void initFruits() {
@@ -35,23 +46,23 @@ public class MainActivity extends AppCompatActivity {
             Fruit apple = new Fruit("Apple", R.drawable.apple_pic);
             fruitList.add(apple);
             Fruit banana = new Fruit("Banana", R.drawable.banana_pic);
-            fruitList.add(apple);
+            fruitList.add(banana);
             Fruit orange = new Fruit("Orange", R.drawable.orange_pic);
-            fruitList.add(apple);
+            fruitList.add(orange);
             Fruit watermelon = new Fruit("Watermelon", R.drawable.watermelon_pic);
-            fruitList.add(apple);
+            fruitList.add(watermelon);
             Fruit pear = new Fruit("Pear", R.drawable.pear_pic);
-            fruitList.add(apple);
+            fruitList.add(pear);
             Fruit grape = new Fruit("Grape", R.drawable.grape_pic);
-            fruitList.add(apple);
+            fruitList.add(grape);
             Fruit pineapple = new Fruit("Pineapple", R.drawable.pineapple_pic);
-            fruitList.add(apple);
+            fruitList.add(pineapple);
             Fruit strawberry = new Fruit("Strawberry", R.drawable.strawberry_pic);
-            fruitList.add(apple);
+            fruitList.add(strawberry);
             Fruit cherry = new Fruit("Cherry", R.drawable.cherry_pic);
-            fruitList.add(apple);
+            fruitList.add(cherry);
             Fruit mango = new Fruit("Mango", R.drawable.mango_pic);
-            fruitList.add(apple);
+            fruitList.add(mango);
 
         }
     }
